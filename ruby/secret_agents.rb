60f38b5db59df0846565ad633ce176443fa875e9
Iliyan Ganev <ganeviliyan@gmail.com>
2017-01-25 17:27:04 -0800

    Write pseudo code

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,16 @@
+
+#Encript
+
+#Write a method with an argument(password)
+#Write a loop to iterate through each letter and replace with the next one.
+
+
+
+
+#Decrypt
+#Write a method that takes an argument(password)
+#Write a loop to iterate through each letter.
+#Replace each letter with the previous one.
+#Declare a variable as the alphabet as a string.
+#Loop through string length and use conditional statement to see which letters 
+#match password letters and return the previous one. 
