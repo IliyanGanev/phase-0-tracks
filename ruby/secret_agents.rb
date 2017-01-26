4bd045c9d47eab71775b048027cdb698842829ed
Iliyan Ganev <ganeviliyan@gmail.com>
2017-01-26 13:18:39 -0800

    Modify hamsters and secret_agents files

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,5 +1,5 @@
 
-#Encript
+#Encrypt
 
 #Write a method with an argument(password)
 #Write a loop to iterate through each letter and replace with the next one.
@@ -14,3 +14,25 @@
 #Declare a variable as the alphabet as a string.
 #Loop through string length and use conditional statement to see which letters 
 #match password letters and return the previous one. 
+
+
+def encrypt(password)
+  counter = 0
+while counter < password.length
+	print password[counter].next
+  counter += 1
+end
+end
+encrypt("sss")
+
+
+
+
+def decrypt(password)
+  counter = 0
+while counter < password.length
+	p=password[counter].next
+	p p
+  counter += 1
+end
+end
