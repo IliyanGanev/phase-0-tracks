7840f2749265f3e54b4e56e95082f2c97766e574
Iliyan Ganev <ganeviliyan@gmail.com>
2017-01-26 20:40:06 -0800

    Gather data on hamsters

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,19 +1,27 @@
 puts "What is the name of the hamster?"
-hamster_name=gets.chomp
+hamster_name = gets.chomp
 puts "What is the volume level?(1 to 10)"
-volume=gets.chomp.to_i
+volume = gets.chomp.to_i
 puts "What is hamster's fur color?"
-color=gets.chomp
+color = gets.chomp
 puts "Is the hamster a good candidate for adoption?(yes/no)"
-candidate=gets.chomp
-puts "What is the hamster's age?"
-age=gets.chomp
-if age=="nil"
-	 "nil"
+candidate = gets.chomp
+if candidate=="yes"
+  candidate = true
 else
-	 age
+  candidate = false
+end
+puts "What is the hamster's age?"
+
+age = gets.chomp
+if age==""
+  age = nil
+else
+  age = age.to_i
 end
 
 puts "The hamster is called #{hamster_name}. Its volume level is #{volume}.
 Its color is #{color}. And the answer to the question Is he good to be adopted is: #{candidate}.
 He is #{age} years old."
+
+
