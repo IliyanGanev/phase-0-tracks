7a8c2bbd0f72bf1ec509117d284fb931e958a237
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-08 14:10:43 -0800

    Add methods for random gender and ethnicity

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -38,41 +38,53 @@ end
 
 
 
-santas = []
+#santas = []
 
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature(unicorn)", "N/A"]
+#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature(unicorn)", "N/A"]
 
-example_genders.length.times do |i|
-	santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+#example_genders.length.times do |i|
+#	santas << Santa.new(example_genders[i], example_ethnicities[i])
+#end
 
 
-santas.each do |santa|
-	santa.speak
-end
+#santas.each do |santa|
+#	santa.speak
+#end
 
-santas[2].eat_milk_and_cookies("Snickers")
+#santas[2].eat_milk_and_cookies("Snickers")
 
-santa = Santa.new("male", "white")
-santa.speak
-santa.eat_milk_and_cookies("Mars")
-puts "Santa has a birthday and he is #{santa.celebrate_birthday} years old"
-santa.gender = "male"
-puts "Santa's gender is #{santa.gender},he is #{santa.ethnicity} and #{santa.age} years old."
-puts santa.get_mad_at("Vixen")
-p santa.reindeer_ranking
+#santa = Santa.new("male", "white")
+#santa.speak
+#santa.eat_milk_and_cookies("Mars")
+#puts "Santa has a birthday and he is #{santa.celebrate_birthday} years old"
+#santa.gender = "male"
+#puts "Santa's gender is #{santa.gender},he is #{santa.ethnicity} and #{santa.age} years old."
+#puts santa.get_mad_at("Vixen")
+#p santa.reindeer_ranking
 
-#Create a variable with randomly selected genders
-#Create a variable for randomly selected ethnicities.
-#Create a variable with random number between 0 and 140.
+#Create a method with randomly selected genders
+#Create a method for randomly selected ethnicities.
 #Create a large number of santas with randomly selected attributes.
 #Print out santas attributes.
 
-rand_gender = example_genders.sample
-p rand_gender
-rand_ethnicity =example_ethnicities.sample
-p rand_ethnicity
-rand_age = rand(140)
-p rand_age
+
+ def rand_gender
+   example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+    gender = example_genders.sample
+  end
+  
+  def rand_ethnicity
+    example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature(unicorn)", "N/A"]
+    ethnicity = example_ethnicities.sample
+  end
+
+
+10.times do
+  new_santa = Santa.new(rand_gender, rand_ethnicity)
+  puts "#{new_santa.gender}  #{new_santa.ethnicity}"
+  
+end
+
+
 
