e5d5a8b16fc49c811288d41dcbe0875cac0332b3
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-08 11:54:53 -0800

    Add driver code to test attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,37 +1,37 @@
 class Santa
 
-attr_reader :age, :ethnicity
-attr_accessor :gender, :reindeer_ranking
+	attr_reader :age, :ethnicity
+	attr_accessor :gender, :reindeer_ranking
+	
+	def initialize(gender, ethnicity)
+		p "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		age = 0
+	
+	end
 
-def initialize(gender, ethnicity)
-	p "Initializing Santa instance..."
-	@gender = gender
-	@ethnicity = ethnicity
-	@age = 0
-	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-age = 0
+	def speak 
+		p "Ho, ho, ho!Haaaappy holidays!"
+		p "Said the #{@ethnicity} #{@gender} santa."
 
-end
+	end
 
-def speak 
-	p "Ho, ho, ho!Haaaappy holidays!"
-	p "Said the #{@ethnicity} #{@gender} santa."
+	def eat_milk_and_cookies(type)
+		p "That was a good #{type}"
+	end
 
-end
+	def celebrate_birthday
+		@age += 1
+	end
 
-def eat_milk_and_cookies(type)
-	p "That was a good #{type}"
-end
-
-def celebrate_birthday
-	@age += 1
-end
-
-def get_mad_at(reindeer_name)
-  	@reindeer_ranking.delete(reindeer_name)
-	@reindeer_ranking << reindeer_name
-	"Sorry #{reindeer_name} but your ranking dropped."
-end
+	def get_mad_at(reindeer_name)
+	  	@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking << reindeer_name
+		"Sorry #{reindeer_name} but your ranking dropped."
+	end
 
 
 end
@@ -60,6 +60,6 @@ santa.eat_milk_and_cookies("Mars")
 
 p "Santa has a birthday and he is #{santa.celebrate_birthday} years old"
 santa.gender = "male"
-p "Santa's gender is #{santa.gender}"
+p "Santa's gender is #{santa.gender},he is #{santa.ethnicity} and #{santa.age} years old."
 p santa.get_mad_at("Vixen")
 p santa.reindeer_ranking
