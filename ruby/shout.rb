cc58b1643fa3eba10dc8ee2a6f5ea1a5ec0a09fe
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-09 13:37:49 -0800

    Add methods and driver code to test them.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,12 @@
-Module Shout
+module Shout
+	def self.yell_angrily(words)
+	words + "!!!" + " :("
+	end
 
-
+	def self.yell_happyly(words)
+		words + ":-)"
+	end
 end
+
+p Shout.yell_angrily("Rrrrr")
+p Shout.yell_happyly("Yeeey")
