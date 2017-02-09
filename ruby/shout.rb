50ffddbb56e1004cdcc5cdec951dbd5c5e532ae8
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-09 15:11:41 -0800

    Comment out module and create mixin with two classes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	def self.yell_angrily(words)
 	words + "!!!" + " :("
@@ -10,3 +11,30 @@ end
 
 p Shout.yell_angrily("Rrrrr")
 p Shout.yell_happyly("Yeeey")
+=end
+
+module Shout
+
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + ":-)"
+	end
+end
+
+
+class Person
+	include Shout
+end
+
+class Singer
+	include Shout
+end
+
+
+
+
+
+
