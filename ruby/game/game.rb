3116f4841470fbd5f2a9095ad2cc82e65c20787f
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-12 11:01:09 -0800

    Find the matching letter method

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,32 +1,38 @@
 class Game
 
-	attr_accessor :word, :guessed_letter, :correct_letters, :guessed_letters, :count, :secret_letters, :l
+	attr_accessor :word, :guessed_letter, :correct_letters, :guessed_letters, :count, :secret_letters
 
 	def initialize(word)
 	  @word = word
 	  @guessed_letter = guessed_letter
 	  @correct_letters = @word.split("")
-	  @guessed_letters = []
-	  @secret_letters = []
-	  @l = @word.tr('a-z', '_') 
-	  @word.length.times{@secret_letters.push("_")}
-	  @count = 0
+		@guessed_letters = []
+		@secret_letters = []
+		@word.length.times{@secret_letters.push("_")}
+		@count = 0
+		
 	end
 
-
-	def guess(word, letter)
-		if
-			word.index(letter)
-			return letter
-		else
-			false
-			@guess_count+=1
-		end
-	end
-
+	
+	
+	def letter_match_array(letter)
+	  if 
+     @correct_letters.include?(letter)
+     @guessed_letters << letter
+    else
+    @count+=1
+    end
+  end
+  
+ 
+  
 end
 
 
-game = Game.new
-p game.guess("abcd", "a")
-p game.guess_count
+game = Game.new ("work")
+
+p game.letter_match_array("r")
+p game.letter_match_array("k")
+p game.guessed_letters
+p game.count
+p game.correct_letters 
