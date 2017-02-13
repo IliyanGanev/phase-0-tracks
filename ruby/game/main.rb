ee0ecd6fddf0514269ae31f3ac623bddadd13265
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-12 23:56:29 -0800

    Transfer pseudocode to file

--- ruby/game/main.rb
+++ ruby/game/main.rb
@@ -2,22 +2,22 @@ require_relative "game"
 
 #Driver code
 
-puts "Enter a word! Your guesses are limited to twice the word length."
+puts "Enter a word!"
 input1 = gets.chomp
 game = Game.new (input1)
 
 loop do
-	puts "Try to guess the letter!"
+	puts "Try to guess the letter! Your guesses are limited to twice the word length."
 	p game.found_letters
 	input2 = gets.chomp
 	input_was_found = game.augment_found_array(input2)
 	p "Wrong answer" if !input_was_found
 	p game.found_letters
 	if game.found_letters == game.correct_letters
-	p "You win!!!"
-    break
+		p "You win!!!"
+    	break
   	elsif game.count == input1.length * 2
-    p "You loose!!!"
-    break
-  end
+    	p "You loose!!!"
+    	break
+  	end
 end
