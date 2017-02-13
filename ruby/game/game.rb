706ea5c519158f60961a6195d1064a62d961cc86
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-12 17:37:52 -0800

    Refacture to use one method for the output

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,36 +1,27 @@
 class Game
 
-	attr_accessor :word, :guessed_letter, :correct_letters, :guessed_letters, :count, :secret_letters
+	attr_accessor :word, :correct_letters, :count, :secret_letters
 
 	def initialize(word)
 	  @word = word
-	  @guessed_letter = guessed_letter
 	  @correct_letters = @word.split("")
-		@guessed_letters = []
-		@secret_letters = []
-		@word.length.times{@secret_letters.push("_")}
-		@count = 0
+	  @secret_letters = []
+	  @word.length.times{@secret_letters.push("_")}
+	  @count = 0
 		
 	end
 
 	
 	
 	def letter_match_array(letter)
-	  if 
-     @correct_letters.include?(letter)
-     @guessed_letters << letter
-    else
-    @count+=1
-    end
-  end
-
- def output(letter)
-    letter_index = @word.index(letter)
-    @secret_letters[letter_index] = letter
-    @secret_letters
-  end
-
-  
+		if @correct_letters.include?(letter)
+			 letter_index = @word.index(letter)
+			 @secret_letters[letter_index] = letter
+			 @secret_letters
+    	else
+    		@count+=1
+    	end
+  	end 
  
   
 end
@@ -38,9 +29,6 @@ end
 
 game = Game.new ("work")
 
+p game.letter_match_array("w")
 p game.letter_match_array("r")
 p game.letter_match_array("k")
-p game.guessed_letters
-p game.count
-p game.correct_letters 
-p game.output("w")
