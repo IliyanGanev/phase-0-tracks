c6360a5c355ef7cb9da48cdcbbcd3b9508d6fd77
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-15 13:34:13 -0800

    initiate js file

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+var colors = ["red", "blue", "yellow", "green"];
+var names = ["Ed", "John", "Bob", "Frank"];
+
+console.log(colors);
+console.log(names);
+
+colors.push("purple");
+console.log(colors);
+
+names.push("Shawn");
+console.log(names);
+
