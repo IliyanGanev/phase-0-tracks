fba0638b5bdd6a4e86c9dc9e61caf494972445d3
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-16 12:01:21 -0800

    Pseudocode

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+//Write a function that reverses a string.
+//Create a for loop that loops backwords while the string length is true.
+//Add driver code to that calls the function with string parameter that has to be reversed.
+//Store the result in a new variable.
+//Add conditional logic that prints the variable if condition evaluates to true.
