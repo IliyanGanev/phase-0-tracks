fccc08be2db8e1aded28de19fbd352dcc2440272
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-15 17:12:54 -0800

    Build an object and work with constructor

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,4 @@
+//Release 0
 var colors = ["red", "blue", "yellow", "green"];
 var names = ["Ed", "John", "Bob", "Frank"];
 
@@ -10,14 +11,14 @@ console.log(colors);
 names.push("Shawn");
 console.log(names);
 
+//Release 1
 var horses = {}
 for (var i = 0; i<names.length; i++){
   horses[names[i]] = colors[i] ;
 }
 console.log(horses);
 
-
-
+//Release 2
 function Car(color, make, type) {
 this.color = color;
 this.make = make;
@@ -34,8 +35,13 @@ var subaru = new Car("red", "Subaru", "AllWheel");
 console.log(subaru.type); 
 subaru.start()
 
+//Release 3
 for (var key in mazda) {
   if (mazda.hasOwnProperty(key)) {
     console.log(key + " -> " + mazda[key]);
   }
 }
+
+//The advantage of constructor functions is that we can create
+// multiple instances of the object. Literal notation is better,
+// if we want to use the object as a simple data container for key value pairs.
