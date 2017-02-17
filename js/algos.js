996cfde37abc431e3c90fe241eb20333fd3d2f20
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-16 22:29:22 -0800

    Update pseudo

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,8 @@
 //Release 0
 //Write a function that takes an array as an argument.
-//Inside that function write a for loop that iterates through each element and compares it with the next one.
-//Compare the elements to the first one an.
+//Inside that function write a for loop that iterates through each element.
+//Declare a variable that equals to the first element.
+//In the loop coompare each element to that variable.If length is longer save it in the same variable.
 //Add driver code to test it.
 
 var arr = ["a", "bcde", "longerphrase"]
@@ -9,8 +10,10 @@ var arr = ["a", "bcde", "longerphrase"]
 	var l = arr[0];
 	
 	for (i = 0; i < arr.length; i ++) {
-	if (arr[i].length > arr[0].length) {
+	if (arr[i].length > l.length) {
 		var l = arr [i];
 		console.log(l)
 	}
+
 }
+console.log(l)
