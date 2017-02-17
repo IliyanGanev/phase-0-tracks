2777743494cfbfaa33068ddde9114221e99c66c4
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-16 17:47:54 -0800

    Write a simple loop

--- js/algos.js
+++ js/algos.js
@@ -3,3 +3,9 @@
 //Inside that function write a for loop that iterates through each element and compares it with the next one.
 //Return the longest phrase from the array.
 //Add driver code to test it.
+
+var arr = ["a", "bcde", "longerphrase"]
+
+for (i = 0; i < arr.length; i ++) {
+	console.log(arr[i]);
+}
