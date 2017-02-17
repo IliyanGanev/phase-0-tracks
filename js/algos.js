91b226b3b44a648bf83db8c45d14dc23108f4158
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-16 22:19:29 -0800

    Change code to get the longest string

--- js/algos.js
+++ js/algos.js
@@ -1,11 +1,16 @@
 //Release 0
 //Write a function that takes an array as an argument.
 //Inside that function write a for loop that iterates through each element and compares it with the next one.
-//Return the longest phrase from the array.
+//Compare the elements to the first one an.
 //Add driver code to test it.
 
 var arr = ["a", "bcde", "longerphrase"]
-
-for (i = 0; i < arr.length; i ++) {
-	console.log(arr[i]);
+	
+	var l = arr[0];
+	
+	for (i = 0; i < arr.length; i ++) {
+	if (arr[i].length > arr[0].length) {
+		var l = arr [i];
+		console.log(l)
+	}
 }
