28d844636fe362d67f8172bd6bff4ac39a04cc68
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-16 17:39:32 -0800

    Write algorithm for sorting loop

---
+++ js/algos.js
@@ -0,0 +1,5 @@
+//Release 0
+//Write a function that takes an array as an argument.
+//Inside that function write a for loop that iterates through each element and compares it with the next one.
+//Return the longest phrase from the array.
+//Add driver code to test it.
