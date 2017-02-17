d2ff68b19c4bfed3c75b1e7b68eb48fdb7fc5eee
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-16 22:43:46 -0800

    Get the longestword

--- js/algos.js
+++ js/algos.js
@@ -2,18 +2,21 @@
 //Write a function that takes an array as an argument.
 //Inside that function write a for loop that iterates through each element.
 //Declare a variable that equals to the first element.
-//In the loop coompare each element to that variable.If length is longer save it in the same variable.
+//In the loop compare each element to that variable. If length is longer save it in the same variable.
 //Add driver code to test it.
 
-var arr = ["a", "bcde", "longerphrase"]
-	
-	var l = arr[0];
-	
-	for (i = 0; i < arr.length; i ++) {
-	if (arr[i].length > l.length) {
-		var l = arr [i];
-		console.log(l)
-	}
 
+
+function longestWord(words)	{
+	var long = words[0];
+	
+	for (i = 0; i < words.length; i ++) {
+		if (words[i].length > words[0].length) {
+			var long = words[i];
+		}	
+	}
+	return(long);
 }
-console.log(l)
+
+var random = ["a", "bcde", "longerphrase"];
+console.log(longestWord(random));
