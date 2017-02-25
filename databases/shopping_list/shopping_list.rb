d67a31454c1095c9d3ecd5429c6284c9d4572fd5
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-24 20:08:35 -0800

    add clear table method

--- databases/shopping_list/shopping_list.rb
+++ databases/shopping_list/shopping_list.rb
@@ -18,7 +18,7 @@ def add_item(db, name, quantity)
 	db.execute("INSERT INTO items (name, quantity) VALUES (?, ?)", [name, quantity])
 end
 
-#add_item(db, "shoes", 2)
+add_item(db, "shoes", 2)
 
 def update_item(db, name, quantity, id)
 	db.execute("UPDATE items SET name=?, quantity=? WHERE id=?", [name, quantity, id])
@@ -30,4 +30,11 @@ def delete_item(db, name)
 	db.execute("DELETE FROM items WHERE name=?", [name])
 end
 
-delete_item(db, "tie")
+#delete_item(db, "tie")
+
+def clear_table(db)
+	db.execute("DELETE FROM items")
+end
+
+#clear_table(db)
+
