940a1a987d8612150efa37dc03a56cc0e1dcc4b8
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-24 17:44:29 -0800

    Add table

--- databases/shopping_list/shopping_list.rb
+++ databases/shopping_list/shopping_list.rb
@@ -3,3 +3,12 @@ require "sqlite3"
 db = SQLite3::Database.new("items.db")
 db.results_as_hash = true
 
+create_table = <<-SQL
+CREATE TABLE IF NOT EXISTS items(
+id INTEGER PRIMARY KEY,
+name VARCHAR(255),
+quantity INT 
+)
+SQL
+
+db.execute(create_table)
