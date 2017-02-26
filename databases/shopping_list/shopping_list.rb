78a64f53bcd0be60b1ab79644a14fa337b5593cf
Iliyan Ganev <ganeviliyan@gmail.com>
2017-02-25 20:55:18 -0800

    Remove unnecessary print updated method

--- databases/shopping_list/shopping_list.rb
+++ databases/shopping_list/shopping_list.rb
@@ -37,59 +37,48 @@ def clear_table(db)
 	db.execute("DELETE FROM items")
 end
 
+#print
 
-
-
-def print_items(db, items)
+def print_items(db)
 	items = db.execute("SELECT * FROM items")
 	puts "\n=========SHOPPING LIST=========\n\n"
 	items.each do |item|
 		puts "item#{item["id"]}: |#{item["name"]}| quantity:#{item["quantity"]}"
 	end	
-
-	puts "\n==============================="
-	
+	puts "\n==============================="	
 end
-	
 
+#print list on startup
 
-
-	def print_updated_item(db, items, id)
-		i = db.execute("SELECT * FROM items WHERE id=?", [id])
-		i.each do |item|
-			puts "Item:#{item['id']} was updated to #{item['name']} quantity:#{item["quantity"]}."
-		end
-		
-	end
-
-
-
-
-items = db.execute("SELECT * FROM items")
-print_items(db, items)
+#items = db.execute("SELECT * FROM items")
+print_items(db)
 
 loop do
-	puts "\nType 'a' to add item,'u' to update, 'd' to delete item and 'c' to clear table." 
-	puts "Type 'q' to quit!\n\n"
+	#puts "\nType 'a' to ADD item, 'u' to UPDATE, 'd' to DELETE item or 'c' to CLEAR the list" 
+	puts "\n SHOPPING LIST MENU"
+	puts "------------------------------------------------------------------"
+	puts "ADD ITEM:type - a   | UPDATE ITEM:type - u | DELETE ITEM:type - d |"
+	puts "PRINT ALL:type - p  | QUIT:type - q        | CLEAR :type - c      |"
+	puts "------------------------------------------------------------------"
 	answer = gets.chomp
 	case answer
 	when 'a'
 		loop do
-			puts "Type an item to add! Type 'done' when done adding items!"
+			puts "Type an item to add! Type 'done' when finished!"
 			item = gets.chomp
 			break if item == 'done'
 			puts "Type ammount"
 			ammount = gets.chomp
 			break if ammount == 'done'
 			add_item(db, item, ammount)
-
 		end
-		print_items(db, items)
+		print_items(db)
 	when "d"
 		puts "Type the number of the item you want to delete!"
 		id = gets.chomp
 		delete_item(db, id) 
 		puts "\nITEM #{id} was DELETED from the list!!!"
+		print_items(db)
 	when "u"
 		puts "Type the number of the item you want to update!"
 		id = gets.chomp
@@ -98,11 +87,14 @@ loop do
 		puts "Type the quantity!"
 		quantity = gets.chomp
 		update_item(db, item, quantity, id)
-		print_updated_item(db, items, id)
+		print_items(db)
 	when "c"
 		clear_table(db)
-	when "q"
-		print_items(db, items)
+		puts "TABLE IS EMPTY!"
+	when "p"
+		print_items(db)
+	when "q" 
+		print_items(db)
 		exit	
 	end
 
