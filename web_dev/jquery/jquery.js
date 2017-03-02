09e6c7efab7ff644afce8520eab2287b3686f84e
Iliyan Ganev <ganeviliyan@gmail.com>
2017-03-02 00:07:20 -0800

    Fix syntax and indention

--- web_dev/jquery/jquery.js
+++ web_dev/jquery/jquery.js
@@ -1,10 +1,10 @@
 //Play with the h2 element 
 
 $(document).ready(function(){
-    $("h2").click(function(){
-        $("span").toggle(1500);
-        $("p").toggle(1500);
-    });
+  $("h2").click(function(){
+    $("span").toggle(1500);
+    $("p").toggle(1500);
+  });
 
 	$("h2").mouseenter(function(){
     	$(this).css("background-color", "#cccccc");
@@ -17,55 +17,53 @@ $(document).ready(function(){
 
 	$("h2").click(function(){
 		$("#pic1").animate({
-      	width: "900px",
-      	height: "250px"
+      width: "900px",
+      height: "250px"
     	}, {
-      	duration: 5000,
-      	easing: "swing",   
-    	});
-  	});
+      duration: 5000,
+      easing: "swing"   
+    });
+  });
 
 
 	$("h2").click(function(){
-    	$("ul").animate({
-     	letterSpacing: "4px"
+    $("ul").animate({
+      letterSpacing: "4px"
     	}, {
-      	duration: 3000,
-      	easing: "swing",
-    	});
+      duration: 3000,
+      easing: "swing"
+    });
 
 
-    	$("ul").animate({
+    $("ul").animate({
      	letterSpacing: "1px"
     	}, {
-      	duration: 1000,
-      	easing: "linear",
-    	});
-		
-  	});
+      duration: 1000,
+      easing: "linear"
+    });
+	});
 
-//Play with pic1 element
+  //Play with pic1 element
 
-  	$("#pic1").mouseenter(function(){
-    	$(this).animate({
-      	width: "800px",
-      	height: "300px"
-    	}, {
-      	duration: 1000,
-      	easing: "swing",   
-    	});
+  $("#pic1").mouseenter(function(){
+    $(this).animate({
+      width: "800px",
+      height: "300px"
+      }, {
+      duration: 1000,
+      easing: "swing"  
+    });
 	});
 
 	$("#pic1").mouseleave(function(){
-    	$(this).animate({
-      	width: "900px",
-      	height: "250px"
+    $(this).animate({
+      width: "900px",
+      height: "250px"
     	}, {
-      	duration: 1000,
-      	easing: "swing",   
-    	});
+      duration: 1000,
+      easing: "swing"   
+    });
 	});
-
 });
 
 
