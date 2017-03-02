8d18c4d4caa558161b1233154fcabd9dfd2ababe
Iliyan Ganev <ganeviliyan@gmail.com>
2017-03-01 22:15:09 -0800

    Add some animations to jquery file

--- web_dev/jquery/jquery.js
+++ web_dev/jquery/jquery.js
@@ -1,6 +1,48 @@
+//Play with the h2 element 
+
 $(document).ready(function(){
     $("h2").click(function(){
-        $("h2").hide();
+        $("span").toggle(1500);
+        $("p").toggle(1500);
     });
+
+$("h2").mouseenter(function(){
+    $(this).css("background-color", "#cccccc");
 });
+
+$("h2").mouseleave(function(){
+    $(this).css("background-color", "#EFF0E9");
+});
+
+
+ $("h2").click(function(){
+    $("#pic1").animate({
+      width: "900px",
+      height: "250px"
+    }, {
+      duration: 5000,
+      easing: "swing",
+     
+    });
+  });
+
+  $("h2").click(function(){
+    $("ul").animate({
+      letterSpacing: "4px"
+    }, {
+      duration: 5000,
+      easing: "swing",
+     
+    });
+  });
+
+
+
+
+
+
+});
+
+
+
 
