36067e109f4141ea04b4346057c772fd05ddcdf0
Iliyan Ganev <ganeviliyan@gmail.com>
2017-03-01 20:00:52 -0800

    Add DOM manpulation script

--- web_dev/chameleon/script.js
+++ web_dev/chameleon/script.js
@@ -1,7 +1,8 @@
+
 console.log("The script is running!");
 
 function addPinkBorder(event) {
-  console.log("click happened! here's the click event:");
+ console.log("click happened! here's the click event:");
   console.log(event);
   event.target.style.border = "2px solid pink";
 }
