a7653ae8832c8480a608809cc2089ac16a48a729
Iliyan Ganev <ganeviliyan@gmail.com>
2017-03-02 22:48:31 -0800

    Add routes

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,51 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+#Release 0:Add Routes
+# write a route that displays an address
+get '/contact/:address' do
+  address = params[:address]
+  "The address is #{address}."
+end
+
+
+#Write a route that takes a name as a query parameter.
+get '/great_job/' do
+  name = params[:name]
+  p params
+  if name
+    "Good job #{name}"
+  else
+    "Good job!"
+  end
+end
+
+
+#Make a route that takes two numbers as parameters adds them and responds with the result.
+get '/:first_number/plus/:second_number' do
+  result = params[:first_number].to_i + params[:second_number].to_i
+  "The result is #{result}."
+end
+
+
+#Make a route that allows the user to search the database in some way.
+get '/search/:name' do
+  name = params[:name]
+  student = db.execute("SELECT * FROM students WHERE name=?", [name])[0]
+  if student == nil
+    "No such student in the database!"
+  else
+    response = ""
+    response << "ID: #{student['id']}<br>"
+    response << "Name:#{student['name']}<br>"
+    response << "Age:#{student['age']}<br>"
+    response << "Campus:#{student['campus']}"
+    response
+  end
+end
+
+
+
+
